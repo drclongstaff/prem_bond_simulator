da330208917056bb2d99cea7e614f5d63316973c
drclongstaff <drclongstaff@gmail.com>
2026-04-21 12:43:32 +0100

    Changes to layout of value boxes and action buttons

---
+++ Leftovers.R
@@ -0,0 +1,27 @@
+#Leftover Value Box Row
+
+## Row {height="5%"}
+
+```{r}
+#| content: valuebox
+#| title: "Prize probablilities"
+#| color: pink
+#| icon: calculator
+p("High to low prizes")
+```
+
+```{r}
+#| content: valuebox
+#| title: "Monthly prizes"
+#| color: green
+#| icon: calendar-day
+p("Enter £ of bonds held")
+```
+
+```{r}
+#| content: valuebox
+#| title: "Long term simulations"
+#| color: blue
+#| icon: hourglass-split
+p("Enter years for simulation")
+```
